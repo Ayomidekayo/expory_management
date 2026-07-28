acb69aff8840a1935bb3e4696a0232e9680bfd26
YourName <your-email@example.com>
2026-07-28 13:10:39 +0100

    remove upload middleware file

--- server/src/config/prisma.ts
+++ server/src/config/prisma.ts
@@ -1,16 +1,13 @@
+
+
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 import dotenv from "dotenv";
-import { PrismaClient } from "../generated";
+import { PrismaClient } from "../generated/client"
 
 dotenv.config();
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
 
-export const prisma = new PrismaClient({
-  adapter,
-});
+export const prisma = new PrismaClient({ adapter });
