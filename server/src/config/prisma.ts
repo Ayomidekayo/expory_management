bd0051e84ab19070d30e6812aca480e9303f3c80
YourName <your-email@example.com>
2026-07-28 12:38:23 +0100

    chanage prisma

--- server/src/config/prisma.ts
+++ server/src/config/prisma.ts
@@ -1,13 +1,16 @@
-
-
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 import dotenv from "dotenv";
-import { PrismaClient } from "../generated/client"
+import { PrismaClient } from "../generated";
 
 dotenv.config();
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
 const adapter = new PrismaPg(pool);
 
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient({
+  adapter,
+});
